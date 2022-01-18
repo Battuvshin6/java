6cb5501be3b9bff9c48e3b175deb17ae7d224769
mstars_lab2_18 <mstarsacademy@MStarss-iMac.local>
2022-01-18 17:28:26 +0800

    upgrade

---
+++ func1.js
@@ -0,0 +1,7 @@
+// let age = prompt("Enter your age:");
+// if (age >= 21){
+//     console.log("Welcome")
+// }
+// else {
+//     console.log("you are not allowed")
+// }
---
+++ function.js
@@ -0,0 +1,40 @@
+// let weekdays = prompt("pick a number")
+
+// if (weekdays === "1")
+// {
+//     console.log( "it is Monday")
+// }
+// else if (weekdays == 2){
+//     console.log( "it is Tuesday")
+// }
+// if (weekdays == 3){
+//     console.log( "it is Wednesday")
+// }
+// if (weekdays == 4){
+//     console.log( "it it Thursday")
+// }
+// if (weekdays == 5){
+//     console.log( "it it Friday")
+// }
+// if (weekdays == 6){
+//     console.log( "it it Saturday")
+// }
+// if (weekdays == 7){
+//     console.log( "it it Sunday")
+// }
+// sondgoi eyreg 13
+// sondgoi sorog -7
+// tegsh eyreg 4
+// tegsh sorog -2
+let number = prompt("Your number:");
+
+if (number % 2 === 0) {
+  if (number < 0) {
+    console.log("sondgoi eyreg");
+  } else {
+    console.log("");
+  }
+} else {
+  if (number % 2) {
+  }
+}
