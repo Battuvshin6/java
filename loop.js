545d69cea7d4a7d34ecc84e700bd4dee28f3050a
mstars_lab2_18 <mstarsacademy@MStarss-iMac.local>
2022-01-25 17:22:18 +0800

    update

---
+++ loop.js
@@ -0,0 +1,43 @@
+// for (let i=0; i<100; i++){
+//     console.log(i);
+// 70-100 hoorondoh 80s ih buh toonii niilberiig olii
+// let total = 1;
+// for (let i = 1; i <= 10; i++) {
+//   total = total * i;
+// }
+// console.log(total);
+// function restaurantMenu() {
+//   document.getElementById("text").innerText =
+//     "Zaidas 6500 \n Undugte huurag 6400";
+// console.log("Tsuivan 5900₮");
+// console.log("Budaatai Huurga 5900₮");
+// console.log("Banshtai Tsai 6500₮");
+// console.log("Piroshki 1500₮");
+// }
+// restaurantMenu();
+// function tomAndToms(coffee) {
+//   return "your " + coffee + " is ready";
+// }
+// let myCoffee = tomAndToms("Latte");
+// console.log(myCoffee);
+
+// 1
+// let num = parseInt(prompt("insert your number: "));
+// function cube(x) {
+//   console.log(x * x * x);
+// }
+// cube(num);
+
+// 2
+// let x = 5;
+// function talbai(x) {
+//   console.log(5 * 2);
+//   console.log(3.14 * (5 * 2));
+//   console.log(2 * 3.14 * 5);
+// }
+// talbai(5);
+
+x = 10;
+y = 8;
+function smallest(x, y) {}
+smallest(10, 8);
