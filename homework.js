deebc88d05f452150a475e91f0a459179be6c590
mstars_lab2_18 <mstarsacademy@MStarss-iMac.local>
2022-01-26 17:32:41 +0800

    up

---
+++ homework.js
@@ -0,0 +1,125 @@
+// let num = prompt("insert number: ");
+// if (num > 0) {
+//   console.log("this number is positive");
+// } else if (num < 0) {
+//   console.log("negitive");
+// }
+
+// console.log("this number is highest");
+// if (num % 7 == 0) {
+//   console.log("dividable by 7");
+// } else {
+//   console.log("not dividable by 7");
+// }
+// if (num % 19 == 0) {
+//   console.log("dividable by 19");
+// } else {
+//   console.log("not dividable by 19");
+// }
+
+// let x = prompt("insert number: ");
+// if (x % 2 == 0) {
+//   console.log("even");
+// } else {
+//   console.log("odd");
+// }
+
+// let x = prompt("insert number: ");
+// if ((x % 4 == 0, x % 400 == 0)) {
+//   console.log("this is leap year");
+// } else {
+//   console.log("this is not leap year");
+// }
+
+// let x = prompt("insert an alphabet: ");
+// if ("a" <= x && x <= "z") {
+//   console.log("it is alpabet");
+// } else {
+//   console.log("it is not alphabet");
+// }
+// if (x === "a") {
+//   console.log("it is constitute");
+// } else if (x === "e") {
+//   console.log("it is constitute");
+// } else if (x === "i") {
+//   console.log("it is constitute");
+// } else if (x === "o") {
+//   console.log("it is constitute");
+// } else if (x === "u") {
+//   console.log("it is constitute");
+// } else if (x === "o") {
+//   console.log("it is constitute");
+// } else {
+//   console.log("It is not constitute");
+// }
+
+// let x = prompt("insert: ");
+// if ("a" <= x && x <= "z") {
+//   console.log("it is an alphabet");
+// } else if (0 < x && x <= 10) {
+//   console.log("it is numbers");
+// }
+
+// let x = prompt("insert: ");
+// if (x === "1") {
+//   console.log("it is Monday");
+// }
+// if (x === "2") {
+//   console.log("it is Tuesday");
+// }
+// if (x === "3") {
+//   console.log("it is Wednesday");
+// }
+// if (x === "4") {
+//   console.log("it is Thursday");
+// }
+// if (x === "5") {
+//   console.log("it is Friday");
+// }
+// if (x === "6") {
+//   console.log("it is Saturday");
+// }
+// if (x === "7") {
+//   console.log("it is Sunday");
+// }
+
+// let x = prompt("insert: ");
+// if (x === "1") {
+//   console.log("it has 31 days");
+// }
+// if (x === "2") {
+//   console.log("it has 28 days");
+// }
+// if (x === "3") {
+//   console.log("it has 31 days");
+// }
+// if (x === "4") {
+//   console.log("it has 30 days");
+// }
+// if (x === "5") {
+//   console.log("it has 31 days");
+// }
+// if (x === "6") {
+//   console.log("it has 30 days");
+// }
+// if (x === "7") {
+//   console.log("it has 31 days");
+// }
+// if (x === "8") {
+//   console.log("it has 31 days");
+// }
+// if (x === "9") {
+//   console.log("it has 30 days");
+// }
+// if (x === "10") {
+//   console.log("it has 31 days");
+// }
+// if (x === "11") {
+//   console.log("it has 30 days");
+// }
+// if (x === "12") {
+//   console.log("it has 31 days");
+// }
+
+let x = prompt("insert: ");
+if(x)
--- loop.js
+++ loop.js
@@ -37,7 +37,66 @@
 // }
 // talbai(5);
 
-x = 10;
-y = 8;
-function smallest(x, y) {}
-smallest(10, 8);
+// x = 10;
+// y = 8;
+// function smallest(x, y) {}
+// smallest(10, 8);
+
+// 2022.01.26
+// let num = parseInt(prompt("insert your number:"));
+// function findSumOfEven(x) {
+//   let total = 0;
+//   for (i = 1; i <= x; i++) {
+//     if (i % 2 == 0) {
+//       total = total + i;
+//     }
+//   }
+//   document.getElementById("text").innerHTML = "your answer: " + total;
+// }
+// console.log(findSumOfEven(num));
+
+let x = parseInt(prompt("insert your number:"));
+let y = parseInt(prompt("insert your number:"));
+function findPower() {
+  //toog n toogoor urjine
+}
+
+// function mathOp() {
+//     function nemeh(x, y) {
+//       let niilber = x + y;
+//       return niilber;
+//     }
+//     function hasah() {
+//       let a = parseInt(prompt("Enter first number: "));
+//       let b = parseInt(prompt("Enter second number: "));
+//       let yalgavar = a - b;
+//       return yalgavar;
+//     }
+//     let choice = prompt(
+//       //choice = '+'
+//       "Enter + to plus operation \nEnter - to subtract\nEnter your choice: "
+//     );
+//     if (choice === "+") {
+//       let a = parseInt(prompt("Enter first number: "));
+//       let b = parseInt(prompt("Enter second number: "));
+//       let sum = nemeh(a, b);
+//       document.getElementById("answer").innerText = sum;
+//     } else if (choice === "-") {
+//       let sub = hasah();
+//       document.getElementById("answer").innerText = sub;
+//     } else {
+//       console.log("wrong choice");
+//     }
+//     //   switch (choice) {
+//     //     case "+":
+//     //       console.log("nemeh uildel hiine");
+//     //       break;
+//     //     case "-":
+//     //       console.log("hasah uildel hiine");
+//     //       break;
+//     //     default:
+//     //       console.log("Wrong choice");
+//     //       break;
+//     //   }
+//   }
+//  mathOp();
