1666a6fc9a030d74ea33caefab5e18664a5cfc92
mstars_lab2_18 <mstarsacademy@MStarss-iMac.local>
2022-02-07 13:51:53 +0800

    updata

---
+++ arrey.js
@@ -0,0 +1,12 @@
+// let myAlphabet = ["a", "b", "c", "d", "e", "f"];
+// function alphabet() {
+//   console.log(myAlphabet.length);
+//   if (myAlphabet < 4) {
+//     console.log("alphabet is under 4");
+//   } else {
+//     console.log("alphabet is over 4");
+//   }
+// }
+// alphabet();
+const Planets = ["Earth", "Pluto", "Jupiter", "Venus", "Saturn"];
+console.log(Planets.indexOf("Earth"));
